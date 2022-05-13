4ee1fa429dae2d7adf2bd688dd1e4bef4e9518eb
Kilivnik <ekstasy1990@gmail.com>
2022-05-14 00:39:32 +0300

    Update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listEl = document.querySelector(`#ingredients`);
+console.log(listEl);
+
+const element = ingredients.map(ingredient => {
+  const item = document.createElement(`li`);
+  item.classList.add(`item`);
+  item.innerHTML = ingredient;
+  listEl.append(item);
+});
+
+
