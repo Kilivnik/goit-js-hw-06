0572461f70dd5ff112749d0e066359a90b65c4b8
Kilivnik <ekstasy1990@gmail.com>
2022-05-18 01:58:11 +0300

    Update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {
+        elements: {email , password},
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert(" Заповніть форму")
+    } else {
+        const formData = {
+            email,
+            password,
+          };
+          console.log(formData);   
+    }
+    console.log(event.currentTarget.elements);
+    event.currentTarget.reset();
+}); 
+
+
 
